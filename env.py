6c98a65b9910688998a53fb744489a0988c99c1c
gooruhh <itzgp981@gmail.com>
2025-01-14 21:53:47 +0530

    Add files via upload

---
+++ env.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+# Load the .env file
+load_dotenv()
+
+# Get the OpenAI API key from environment variable
+openai.api_key = os.getenv(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
+)
